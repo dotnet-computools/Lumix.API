2dd112c32eebae9cf39928070aacfa77f799f47d
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-12-16 18:06:12 +0200

    Create LumixDbContext.cs

---
+++ Lumix.Persistence/LumixDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Lumix.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lumix.Persistence
+{
+	public class LumixDbContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Photo> Photos { get; set; }
+		public DbSet<Like> Likes { get; set; }
+		public DbSet<Comment> Comments { get; set; }
+		public DbSet<Follow> Follows { get; set; }
+		public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer();
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(LumixDbContext).Assembly);
+		}
+	}
+}
